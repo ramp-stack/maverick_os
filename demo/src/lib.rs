a995d1adcfcf6c8fce6b5b0030785359a24a5afb
CalebCouch <calebblackcat@gmail.com>
2026-04-30 11:48:10 +0200

    test

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,20 +1,25 @@
 use maverick_os::{Application, Context, start};
-use maverick_os::air::{Contracts, Contract, Substance, Id, Reactants, Reactant, Beaker, Name};
+use maverick_os::air::{self, Contracts, Contract, Substance, Id, Reactants, Reactant, Beaker, Name};
 use maverick_os::window::{self, Input, KeyEvent, Renderer, Handle};
+use maverick_os::runtime::{Services, Service, async_trait};
 
 use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 use std::convert::Infallible;
+use std::time::Duration;
 
 use serde::{Serialize, Deserialize};
 
-//  pub struct ChatBot;
-//  impl Service for ChatBot {
-//      async fn run(&mut self, ctx: &mut air::Context) -> Option<Duration> {
-//          ctx.query.
-//          Some(Duration::from_millis(1))
-//      }
-//  }
+pub struct ChatBot;
+#[async_trait]
+impl Service for ChatBot {
+    async fn run(&mut self, ctx: &mut air::Context) -> Option<Duration> {
+        ctx.list(&ChatRoom::id()).into_iter().for_each(|id| {
+            ctx.send(id, "/messages", SendMessage("This is an automated message from the chat bot sent every second: 'Keep It Quiet!'".to_string())).unwrap();
+        });
+        Some(Duration::from_secs(5))
+    }
+}
 
 #[derive(Serialize, Deserialize, Hash)]
 pub struct ChatRoom;
@@ -22,12 +27,12 @@ impl ChatRoom {
     pub fn new(_name: &str) -> Self {ChatRoom}
 }
 impl Contract for ChatRoom {
-    fn id() -> Id {Id::hash("ChatRoom2.4")}
+    fn id() -> Id {Id::hash("ChatRoom2.7")}
 
     fn init(self, signer: &Name, _timestamp: u64) -> Substance {Substance::Map(BTreeMap::from([
         ("name".to_string(), Substance::String("myroom".to_string())),
         ("author".to_string(), Substance::String(signer.to_string())),
-        ("messages".to_string(), Substance::map())
+        ("messages".to_string(), Substance::Seq(Vec::new()))
     ]))}
 
     fn routes() -> BTreeMap<PathBuf, Reactants> {
@@ -97,6 +102,7 @@ impl Application for DemoApplication {
     }
     
     fn contracts() -> Contracts {Contracts::new().add::<ChatRoom>()}
+    fn services() -> Services {vec![Box::new(ChatBot)]}
 }
 
 start!(DemoApplication);
