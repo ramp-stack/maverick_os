778d5ce602561df506e25cb4723167b66929d7e2
CalebCouch <calebblackcat@gmail.com>
2026-06-11 13:08:47 +0200

    simplfied demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,44 +1,11 @@
 use maverick_os::{Application, Context, start};
-use maverick_os::air::{self, Contract, Reactants, Reactant, Instance, Name, Service, Services, Update};
+use maverick_os::air::{self, Contract, Reactants, Reactant, Instance, Name, Service, Services, Listner};
 use maverick_os::air::names::Id;
 use maverick_os::window::{self, Input, KeyEvent, Renderer, Handle};
-//use maverick_os::runtime::{Services, Service, async_trait};
 
-use std::collections::BTreeMap;
 use std::time::Duration;
 
 use serde::{Serialize, Deserialize};
-use tokio::select;
-use futures_util::stream::FuturesUnordered;
-use futures_util::StreamExt;
-
-
-pub struct Listner<C: Contract>(BTreeMap<Id, Instance<C>>);
-impl<C: Contract> Listner<C> {
-    pub async fn listen(&mut self, ctx: &mut air::Context) -> (&mut Instance<C>, Option<Update>) {
-        if self.0.is_empty() && let Some(room) = ctx.listen().await.as_contract::<C>() {
-            (self.0.entry(room.id()).or_insert(room), None)
-        } else { loop {
-            let mut pending = self.0.values_mut().map(|instance| async {
-                (instance.listen_confirmed().await, instance.id())
-            }).collect::<FuturesUnordered<_>>();
-
-            select! { biased;
-                instance = ctx.listen() => {
-                    drop(pending);
-                    if let Some(room) = instance.as_contract::<C>() {
-                        break (self.0.entry(room.id()).or_insert(room), None)
-                    }
-                },
-                Some((update, id)) = pending.next() => {
-                    drop(pending);
-                    break (self.0.get_mut(&id).unwrap(), Some(update));
-                },
-            }
-        }}
-    }
-}
-impl<C: Contract> Default for Listner<C> {fn default() -> Self {Self(BTreeMap::default())}}
 
 #[derive(Default)]
 pub struct ChatBot(Listner<Room>);
